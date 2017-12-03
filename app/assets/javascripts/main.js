7316e4ea2768425334b7092cf60b1c6a5fef0594
Jared Gray <jared@jaredgray.us>
2017-12-03 16:14:05 -0700

    Implement smooth scroll

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -33,3 +33,11 @@
     displayTypeSpeed: 100
   });
 })(jQuery, window, document);
+
+$(document).on('click', 'a[href^="#"]', function (event) {
+  event.preventDefault();
+
+  $('html, body').animate({
+    scrollTop: $($.attr(this, 'href')).offset().top
+  }, 500);
+});
