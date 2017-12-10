cace1a604f21fe2a5e88e07c525230d0a937bbd3
Jared Gray <jared@jaredgray.us>
2017-12-10 16:08:32 -0700

    Enable compression

--- config/application.rb
+++ config/application.rb
@@ -12,8 +12,7 @@ module RjMurphyConstruction
     config.load_defaults 5.1
 
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+
+    config.middleware.use Rack::Deflater
   end
 end
