1b9d9aa10d4025ca0976d29598d36bec4b3e487e
Jared Gray <jared@jaredgray.us>
2017-12-10 19:39:32 -0700

    Remove owl and use assets gem

--- Gemfile
+++ Gemfile
@@ -50,4 +50,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 source 'https://rails-assets.org' do
   gem 'rails-assets-jquery.easing'
   gem 'rails-assets-wow'
+  gem 'rails-assets-OwlCarousel2'
 end
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,8 +5,8 @@
 //= require bootstrap
 //= require jquery.easing
 //= require wow
+//= require OwlCarousel2
 //= require externals/dzsparallaxer
-//= require externals/owl.carousel.min.js
 //= require externals/custom
 //= require externals/masterslider.min.js
 //= require externals/master-custom
