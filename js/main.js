91782133e0e8a7af9fcbee600b1a7a19a3c7734c
Jared Gray <jared@jaredgray.us>
2018-02-17 19:46:12 -0700

    Complete redo of rails site to static

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 (function($) {
+  $(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+  });
+
   $(".service-slider").owlCarousel({
     loop: true,
     margin: 10,
